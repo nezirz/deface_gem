94f040082c14deecdbd3a591a20af4272c1547cf
nezir <nezir.zahirovic@gmail.com>
2018-09-25 14:53:51 +0200

    Initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+  def index
+    @lat = "43.8563"
+    @long = "18.4131"
+  end
+end
---
+++ app/overrides/index.rb
@@ -0,0 +1,4 @@
+Deface::Override.new(:virtual_path => "home/index", 
+                     :name => "index", 
+                     :replace => "h1#someh1", 
+                     :text => "<h1>New Post</h1>")
